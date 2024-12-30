0da86c78fd8da424facfb38fc744aa5742b2e818
raselsabardila <bautai433@gmail.com>
2024-12-30 10:35:32 +0700

    <setup>(Dashboard): SEtup laravel and template dashboard.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('dashboards.templates.core')
+
+@section('title-page')
+    Dashboard
+@endsection
+
+@section('breadcumb')
+    <li class="breadcrumb-item active">Dashboard</li>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores tenetur nihil quis. Natus corrupti deleniti beatae consequatur quisquam possimus temporibus. Hic dolor corrupti itaque quisquam, ipsam vel eius sint.
+    </div>
+@endsection
